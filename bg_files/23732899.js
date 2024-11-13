d6a0e557219dd43f4d168d3148f61c2da0a968f8
Branded UX <32825782+brandedux@users.noreply.github.com>
2024-11-13 10:11:04 -0800

    Add files via upload

---
+++ bg_files/23732899.js
@@ -0,0 +1,17 @@
+_iub.csRC = { consApiKey: 'hFJQaKEY5zgJKbEfvaWlOK8nnHLsKN9p', consentDatabasePublicKey: 'bGF3wj9x5y8yOKY2v38W5yUiyU1Gwihf' }
+_iub.csEnabled = true;
+_iub.csPurposes = [5,3,1,4];
+_iub.tcfV = [755,804];
+_iub.cpUpd = 1731060375;
+_iub.csFeatures = {
+  geolocation_setting: true,
+  cookie_solution_white_labeling: 1,
+  rejection_recovery: false,
+  full_customization: true,
+  multiple_languages: true,
+  mobile_app_integration: false
+};
+_iub.csT = null;
+_iub.googleConsentModeV2 = true;
+_iub.totalNumberOfProviders = 9;
+_iub.csSiteConf = {"applyGdprForCH":false,"askConsentAtCookiePolicyUpdate":true,"countryDetection":true,"enableFadp":true,"enableLgpd":true,"enableTcf":true,"enableUspr":true,"floatingPreferencesButtonDisplay":"bottom-right","gdprAppliesGlobally":false,"googleAdditionalConsentMode":true,"lang":"en","lgpdAppliesGlobally":false,"perPurposeConsent":true,"siteId":3494181,"storage":{"useSiteId":true},"tcfPurposes":{"2":"consent_only","7":"consent_only","8":"consent_only","9":"consent_only","10":"consent_only","11":"consent_only"},"whitelabel":false,"cookiePolicyId":23732899,"banner":{"acceptButtonCaptionColor":"#FFFFFF","acceptButtonColor":"#0073CE","acceptButtonDisplay":true,"backgroundColor":"#FFFFFF","closeButtonDisplay":false,"customizeButtonCaptionColor":"#4D4D4D","customizeButtonColor":"#DADADA","customizeButtonDisplay":true,"explicitWithdrawal":true,"listPurposes":true,"logo":null,"ownerName":"www.premai.io/","position":"float-top-center","rejectButtonCaptionColor":"#FFFFFF","rejectButtonColor":"#0073CE","rejectButtonDisplay":true,"showTitle":false,"showTotalNumberOfProviders":true,"textColor":"#000000"}};
